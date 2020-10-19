8fd94a9ad8f295f45f0107987308b28fa37c829f
Prabath03Dot <48862955+Prabath03Dot@users.noreply.github.com>
2020-10-19 11:37:41 +0530

    first commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int k,j,r;
+    printf("enter the rows\n");
+    scanf("%d",&r);
+    printf("\n\n");
+
+    for (k=0;k<r;k++){
+            for(j=0;j<=k;j++)
+            {
+                printf("* ");
+            }
+        printf("\n");
+    }
+
+
+    return 0;
+}
